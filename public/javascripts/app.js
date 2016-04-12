4bd9e5962601d2a942c77df8b0ef7f84ca533bdd
Dylan Rowe <dylanmrowe@gmail.com>
2016-04-12 07:28:46 +0000

    No longer can be invisible, there's still a weird bug if you shoot too fast

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -45,7 +45,7 @@ app.controller('ctrl', function ($scope, $interval, Player, socket, $http) {
         size: 40,
         speed: 4,
         shotSpeed: 1,
-        shotSize: 10,
+        shotSize: 15,
         freq: 7,
         range: 3,
         killstreak: 0
@@ -187,16 +187,19 @@ app.controller('ctrl', function ($scope, $interval, Player, socket, $http) {
 
     socket.on('youHit', function() {
         $scope.player.hp--;
-        $scope.player.width = $scope.player.hp*8;
-        $scope.player.height = $scope.player.hp*8;
 
-        if($scope.player.hp == 2){
-            console.log("small-power-time!!!");
-            $scope.user.range = .5;
-            $scope.user.speed = 5;
-            $scope.user.shotSpeed = 3;
-            $scope.user.shotSize = 6;
-            //$scope.user.freq = 5;
+        if( $scope.player.hp != 0){
+            $scope.player.width = $scope.player.hp*8;
+            $scope.player.height = $scope.player.hp*8;
+
+            if($scope.player.hp == 2){
+                console.log("small-power-time!!!");
+                $scope.user.range = .5;
+                $scope.user.speed = 5;
+                $scope.user.shotSpeed = 3;
+                $scope.user.shotSize = 6;
+                //$scope.user.freq = 5;
+            }
         }
     })
 
